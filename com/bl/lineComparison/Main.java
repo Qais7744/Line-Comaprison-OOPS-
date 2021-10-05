5b32eec21795527e761d604492ec4d7e66150aac
unknown <altamashbagwan1@gmail.com>
2021-10-05 23:28:37 +0530

    Added: Functioning Of Calculate Length of Distance

---
+++ com/bl/lineComparison/Main.java
@@ -0,0 +1,23 @@
+package com.bl.lineComparison;
+
+public class Main {
+	
+	public static void main(String[] args) {
+
+		Point p1 = new Point();
+		p1.x = 6;
+		p1.y = 7; 
+
+		Point p2 = new Point();
+		p2.x = 4;
+		p2.y = 5;
+
+		Line l1 = new Line();
+		l1.p1 = p1;
+		l1.p2 = p2;
+
+		float x = l1.getLength();
+		System.out.println("Actual length value : " + x);
+	}
+
+}
---
+++ com/bl/lineComparison/Point.java
@@ -0,0 +1,12 @@
+package com.bl.lineComparison;
+
+public class Point {
+
+	public int x;
+	public int y;
+	
+}
+
+
+
+
