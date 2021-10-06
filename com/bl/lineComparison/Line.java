51b0f73e6873e7848b9645f6ab3fe27b135d9d23
unknown <altamashbagwan1@gmail.com>
2021-10-06 23:54:02 +0530

    Added: Functioning Of Calculate Length of Distance Two And Check Equal or Not

--- com/bl/lineComparison/Line.java
+++ com/bl/lineComparison/Line.java
@@ -7,4 +7,13 @@ public class Line {
 		return (float) Math.sqrt(Math.pow(p1.x - p2.x, 2) + Math.pow(p1.y - p2.y, 2));
 	}
 	
+	@Override
+	public String toString() {
+		return "Line [p1=" + p1 + ", p2=" + p2 + ", p3=" + p3 + ", p4=" + p4 + "]";
+	}
+
+	public float getLengthlineTwo() {
+		return (float) Math.sqrt(Math.pow(p3.x -p4.x, 2) + Math.pow(p3.y - p4.y, 2));
+	}
+	
 }
